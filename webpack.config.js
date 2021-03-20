f1cbfaaa2259529efde79fbb2db420acd4063e67
Brian Palmer <dreamsandcode@gmail.com>
2021-03-20 17:56:07 -0400

    Added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
         test: /\.(png|jpg)$/,
         use: ["file-loader"],
       },
