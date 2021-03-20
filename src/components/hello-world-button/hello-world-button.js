6afb340e004aca5b34250bafd2d73232bfc1082d
Brian Palmer <dreamsandcode@gmail.com>
2021-03-20 00:01:34 -0400

    Handling css with webpack

---
+++ src/components/hello-world-button/hello-world-button.js
@@ -0,0 +1,18 @@
+import "./hello-world-button.css";
+class HelloWorldButton {
+  render() {
+    const button = document.createElement("button");
+    button.innerHTML = "Hello World";
+    button.classList.add("hello-world-button");
+    button.onclick = function () {
+      const p = document.createElement("p");
+      p.innerHTML = "Hello World";
+      p.className = "hello-world-text";
+      body.appendChild(p);
+    };
+    const body = document.querySelector("body");
+    body.appendChild(button);
+  }
+}
+
+export default HelloWorldButton;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "http://dreamsandcode.com/",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
